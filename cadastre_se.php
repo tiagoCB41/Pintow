ead96508b207223d49914a781b69d89ff5724c12
Cairo <cairofelipedev@gmail.com>
2023-03-03 11:19:03 -0300

    W-full button

--- cadastre_se.php
+++ cadastre_se.php
@@ -77,7 +77,7 @@ require "config/url.class.php";
             <div class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-blue-600"></div>
           </label>
         </div>
-        <input name="btnsave" type="submit" class="my-4 cursor-pointer rounded bg-color1 py-3 px-6 font-medium  text-white duration-300 ease-in-out hover:bg-blue-apcef" value="Finalizar Cadastro" />
+        <input name="btnsave" type="submit" class="my-4 cursor-pointer rounded bg-color1 py-3 px-6 font-medium  text-white duration-300 ease-in-out hover:bg-blue-apcef w-full" value="Finalizar Cadastro" />
       </form>
     </div>
   </div>
